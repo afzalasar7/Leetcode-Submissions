dc67530e41d5cb07c224c48489efb211a8f4aa2d
AFZAL ASAR <111232340+afzalasar7@users.noreply.github.com>
2023-09-09 23:05:04 +0530

    Time: 48 ms (16.28%) | Memory: 16.1 MB (99.84%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,23 +1,30 @@
 class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+#     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+#         res = []
+#         left, right = 0, len(matrix[0])
+#         top, down = 0, len(matrix)
+
+#         while left < right and top < down:
+#             for i in range(left, right):
+#                 res.append(matrix[top][i])
+#             top += 1
+#             for i in range(top, down):
+#                 res.append(matrix[i][right - 1])
+#             right -= 1
+#             if not (left < right and top < down):
+#                 break
+#             for i in range(right - 1, left - 1, -1):
+#                 res.append(matrix[down - 1][i])
+#             down -= 1
+#             for i in range(down - 1, top - 1, -1):
+#                 res.append(matrix[i][left])
+#             left += 1
+
+#         return res
+
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:            
         res = []
-        left, right = 0, len(matrix[0])
-        top, down = 0, len(matrix)
-
-        while left < right and top < down:
-            for i in range(left, right):
-                res.append(matrix[top][i])
-            top += 1
-            for i in range(top, down):
-                res.append(matrix[i][right - 1])
-            right -= 1
-            if not (left < right and top < down):
-                break
-            for i in range(right - 1, left - 1, -1):
-                res.append(matrix[down - 1][i])
-            down -= 1
-            for i in range(down - 1, top - 1, -1):
-                res.append(matrix[i][left])
-            left += 1
-
+        while matrix:
+            res.extend(matrix.pop(0))
+            matrix = [*zip(*matrix)][::-1]
         return res
