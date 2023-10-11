f2c6903c4ed1f6729566c00fd680cec6333d93cd
AFZAL ASAR <111232340+afzalasar7@users.noreply.github.com>
2023-10-11 21:45:41 +0530

    Time: 63 ms (48.03%) | Memory: 16.3 MB (21.91%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,4 +1,21 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        #number = str(x)
+        # length = len(number)
+        # for i in range(len(number)//2):
+        #     if number[i] != number[length -1-i]:
+        #         return False
+        # return True
+        # if len(number) %2==0:
+        #     return True if number[:len(number)//2] == number[(len(number)//2):len(number)] else False
+        # else:
+        #     return True if number[:len(number)//2] == number[(len(number)//2)+1:len(number)] else False
+        # return True if number[:len(number)//2] == number[len(number)-1:(len(number)//2)+1:-1] else False
 
-        return True if str(x) == str(x)[::-1] else False
+        rev = 0
+        y = x
+        while y>0:
+            rev = rev*10 + y%10
+            y = y// 10
+
+        return True if x == rev else False
