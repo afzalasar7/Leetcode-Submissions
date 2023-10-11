5f5948067ccfe34ce193aeef1d2463f298c2a00f
AFZAL ASAR <111232340+afzalasar7@users.noreply.github.com>
2023-10-11 21:21:44 +0530

    Time: 50 ms (7.38%) | Memory: 16.2 MB (76.43%) - LeetSync

--- 231-power-of-two/power-of-two.py
+++ 231-power-of-two/power-of-two.py
@@ -1,6 +1,19 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         power = [2**x for x in range (31)]
-        if n in power:
-            return True
+        high = len(power)-1
+        low = 0
+        while True:
+            mid = (high + low)//2
+            if power[mid] == n:
+                return True
+            if high == low:
+                return 0
+            elif power[mid] > n:
+                high = mid - 1
+            elif power[mid] < n:
+                low = mid + 1 
+            
+        # if n in power:
+        #     return True
         
