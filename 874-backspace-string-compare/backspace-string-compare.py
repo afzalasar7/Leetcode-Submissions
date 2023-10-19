413bcfd6e50f790c3f8afccf725554c9be04d289
AFZAL ASAR <111232340+afzalasar7@users.noreply.github.com>
2023-10-19 19:29:08 +0530

    Time: 41 ms (43.68%) | Memory: 16.3 MB (54.52%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.py
+++ 874-backspace-string-compare/backspace-string-compare.py
@@ -1,14 +1,32 @@
+# class Solution:
+#     def backspaceCompare(self, s: str, t: str) -> bool:
+#         s = self.processString(s)
+#         t = self.processString(t)
+#         return s == t
+    
+#     def processString(self, text: str) -> str:
+#         result = []
+#         for char in text:
+#             if char != '#':
+#                 result.append(char)
+#             elif result:
+#                 result.pop()  # Remove the last character if there is one
+#         return ''.join(result)
+
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        s = self.processString(s)
-        t = self.processString(t)
-        return s == t
-    
-    def processString(self, text: str) -> str:
-        result = []
-        for char in text:
-            if char != '#':
-                result.append(char)
-            elif result:
-                result.pop()  # Remove the last character if there is one
-        return ''.join(result)
+        res1 = []
+        res2 = []
+        for c in s:
+            if c=="#":
+                if res1:
+                    res1.pop()
+            else:
+                res1.append(c)
+        for c in t:
+            if c=="#":
+                if res2:
+                    res2.pop()
+            else:
+                res2.append(c)
+        return res1==res2
