e7d9ff9f49ec86caccdca8203332590f95798b84
AFZAL ASAR <111232340+afzalasar7@users.noreply.github.com>
2023-10-23 22:26:40 +0530

    Time: 43 ms (82.73%) | Memory: 16.6 MB (79.77%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -12,23 +12,46 @@
 #             elif nums[mid]<target:
 #                 start= mid+1
 #         return -1
+# class Solution:
+#     def search(self, nums: List[int], target: int) -> int:
+#         start, end = 0, len(nums) - 1
+#         while start <= end:
+#             mid = (start + end) // 2
+#             if nums[mid] == target:
+#                 return mid
+#             elif nums[mid] >= nums[start]:
+#                 # Check if left half is sorted and if the target is in that range.
+#                 if nums[start] <= target < nums[mid]:
+#                     end = mid - 1
+#                 else:
+#                     start = mid + 1
+#             else:
+#                 # Right half is sorted and if the target is in that range.
+#                 if nums[mid] < target <= nums[end]:
+#                     start = mid + 1
+#                 else:
+#                     end = mid - 1
+#         return -1
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        start, end = 0, len(nums) - 1
-        while start <= end:
-            mid = (start + end) // 2
-            if nums[mid] == target:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if target == nums[mid]:
                 return mid
-            elif nums[mid] >= nums[start]:
-                # Check if left half is sorted and if the target is in that range.
-                if nums[start] <= target < nums[mid]:
-                    end = mid - 1
+
+            # left sorted portion
+            if nums[l] <= nums[mid]:
+                if target > nums[mid] or target < nums[l]:
+                    l = mid + 1
                 else:
-                    start = mid + 1
+                    r = mid - 1
+            # right sorted portion
             else:
-                # Right half is sorted and if the target is in that range.
-                if nums[mid] < target <= nums[end]:
-                    start = mid + 1
+                if target < nums[mid] or target > nums[r]:
+                    r = mid - 1
                 else:
-                    end = mid - 1
+                    l = mid + 1
         return -1
