31517e186a76ea251a9583897e07227f1ab0ba0d
AFZAL ASAR <111232340+afzalasar7@users.noreply.github.com>
2023-10-27 21:56:32 +0530

    Time: 84 ms (98.89%) | Memory: 16.3 MB (54.45%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,33 +1,41 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        # Check if the input string is empty, return an empty string if so
-        if not s:
-            return ""
+        # Step 1: Preprocess the input string
+        processed_str = "^#"
+        for c in s:
+            processed_str += c + "#"
+        processed_str += "$"
 
-        # Initialize variables to store the indices of the longest palindrome found
-        longest_palindrome_indices = [0, 0]
+        # Step 2: Initialize variables for the algorithm
+        str_length = len(processed_str)
+        palindrome_lengths = [0] * str_length
+        center = 0  # Current center of the palindrome
+        right_edge = 0  # Rightmost edge of the palindrome
 
-        def expand_around_center(s, i, j):
-            # Helper function to find and return the indices of the longest palindrome
-            # extended from s[i..j] (inclusive) by expanding around the center
-            while i >= 0 and j < len(s) and s[i] == s[j]:
-                i -= 1
-                j += 1
-            return [i + 1, j - 1]
+        # Step 3: Loop through the modified string to find palindromes
+        for i in range(1, str_length - 1):
+            palindrome_lengths[i] = min(right_edge - i, palindrome_lengths[2 * center - i]) if right_edge > i else 0
 
-        for i in range(len(s)):
-            current_indices = expand_around_center(s, i, i)
+            # Expand the palindrome around the current character
+            while processed_str[i + 1 + palindrome_lengths[i]] == processed_str[i - 1 - palindrome_lengths[i]]:
+                palindrome_lengths[i] += 1
 
-            # Compare the length of the current palindrome with the longest found so far
-            if current_indices[1] - current_indices[0] > longest_palindrome_indices[1] - longest_palindrome_indices[0]:
-                longest_palindrome_indices = current_indices
+            # Update the rightmost edge and center if necessary
+            if i + palindrome_lengths[i] > right_edge:
+                center = i
+                right_edge = i + palindrome_lengths[i]
 
-            if i + 1 < len(s) and s[i] == s[i + 1]:
-                even_indices = expand_around_center(s, i, i + 1)
+        # Step 4: Find the longest palindrome and its center
+        max_length = 0
+        max_center = 0
+        for i in range(str_length):
+            if palindrome_lengths[i] > max_length:
+                max_length = palindrome_lengths[i]
+                max_center = i
 
-                # Compare the length of the even-length palindrome with the longest found so far
-                if even_indices[1] - even_indices[0] > longest_palindrome_indices[1] - longest_palindrome_indices[0]:
-                    longest_palindrome_indices = even_indices
+        # Step 5: Extract the longest palindrome from the modified string
+        start = (max_center - max_length) // 2
+        end = start + max_length
 
-        # Extract and return the longest palindrome substring using the indices
-        return s[longest_palindrome_indices[0]:longest_palindrome_indices[1] + 1]
+        # Return the longest palindrome in the original string
+        return s[start:end]
