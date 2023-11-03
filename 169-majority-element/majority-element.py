e81575aa1f587cb2405644617598701d652505e4
AFZAL ASAR <111232340+afzalasar7@users.noreply.github.com>
2023-11-03 09:52:52 +0530

    Time: 148 ms (69.93%) | Memory: 17.7 MB (76.05%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,20 +1,9 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         
-        # from collections import Counter
-        # hashMap = Counter(nums)
-        # for i in hashMap:
-        #     if hashMap[i] > len(nums)//2:
-        #         return i
-
-        # res,count = 0,0
-        # for i in nums:
-        #     if count == 0:
-        #         res = i
-        #     count+= 1 if i == res else -1
-        # return res
-        major = len(nums)/2
-
-        for num in set(nums):
-            if nums.count(num) > major:
-                return num
+        from collections import Counter
+        hash = Counter(nums)
+        major = len(nums)//2
+        for i, v in hash.items():
+            if v>major:
+                return i
