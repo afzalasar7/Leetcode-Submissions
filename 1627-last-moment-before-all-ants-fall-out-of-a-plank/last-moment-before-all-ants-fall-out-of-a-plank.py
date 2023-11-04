184c3379bfa0add24467811d8d0284c81234d31e
AFZAL ASAR <111232340+afzalasar7@users.noreply.github.com>
2023-11-04 21:22:49 +0530

    Time: 142 ms (84.89%) | Memory: 17.2 MB (67.63%) - LeetSync

--- 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.py
+++ 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.py
@@ -1,10 +1,5 @@
 class Solution:
     def getLastMoment(self, n: int, left: List[int], right: List[int]) -> int:
-        # Find the farthest position among ants moving to the left.
-        maxLeft = max(left, default=0)
-
-        # Find the nearest position among ants moving to the right.
-        minRight = n - min(right, default=n)
-
-        # Calculate the time when the last ant(s) fall.
-        return max(maxLeft, minRight)
+        ans = max(max(left) if left else 0,n-min(right) if right else 0)
+        return ans
+        
