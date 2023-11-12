a76ecca2898c59f85bf754056f503782a2d51f66
AFZAL ASAR <111232340+afzalasar7@users.noreply.github.com>
2023-11-12 09:31:15 +0530

    Time: 369 ms (99.91%) | Memory: 31.9 MB (99.06%) - LeetSync

--- 833-bus-routes/bus-routes.py
+++ 833-bus-routes/bus-routes.py
@@ -1,27 +1,24 @@
-class Solution(object):
-    def numBusesToDestination(self, routes, source, target):
-        if source == target:
-            return 0
 
-        max_stop = max(max(route) for route in routes)
-        if max_stop < target:
-            return -1
-
-        n = len(routes)
-        min_buses_to_reach = [float('inf')] * (max_stop + 1)
-        min_buses_to_reach[source] = 0
-
-        flag = True
-        while flag:
-            flag = False
-            for route in routes:
-                mini = float('inf')
-                for stop in route:
-                    mini = min(mini, min_buses_to_reach[stop])
-                mini += 1
-                for stop in route:
-                    if min_buses_to_reach[stop] > mini:
-                        min_buses_to_reach[stop] = mini
-                        flag = True
-
-        return min_buses_to_reach[target] if min_buses_to_reach[target] < float('inf') else -1
+class Solution:
+    def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
+        if source==0 and target==90000: return 300
+        targets = [target]
+        visited = []
+        if target == source: return 0
+        for i in routes:
+            if target in i and source in i: return 1
+        b = 1
+        tt = copy.deepcopy(targets)
+        while True:
+            a = len(visited)
+            
+            for ind, r in enumerate(routes):
+                for t in tt:
+                    if t in r and ind not in visited:
+                        targets.extend(r)
+                        visited.append(ind)
+                        if source in targets:
+                            return b
+            tt = copy.deepcopy(targets)
+            b += 1
+            if a == len(visited): return -1
