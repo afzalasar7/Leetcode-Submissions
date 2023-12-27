6826a62ca93779fd17d671a93b442a934fdc04f8
AFZAL ASAR <111232340+afzalasar7@users.noreply.github.com>
2023-12-27 12:06:30 +0530

    Time: 122 ms (99.73%) | Memory: 18.8 MB (6.59%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -2,7 +2,14 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         
         from collections import Counter
-        hash = Counter(nums)
+        hash = {}
+
+        for i in nums:
+            hash[i] = hash.get(i, 0) +1
+
+
+        
+            
         major = len(nums)//2
         for i, v in hash.items():
             if v>major:
