877d0c8a0d9f9e2fe7638dc14bcfc725a1c7702f
AFZAL ASAR <111232340+afzalasar7@users.noreply.github.com>
2023-12-27 12:21:08 +0530

    Time: 123 ms (99.67%) | Memory: 18.9 MB (6.59%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,21 +1,26 @@
 class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        
-        from collections import Counter
-        hash = {}
-
+    def majorityElement(self, nums: List[int]) -> int:        
+    #    tc = O(n), sc = O(n)
+    #    from collections import Counter
+    #     hash = {}
+    #     for i in nums:
+    #         #hash[i] = hash.get(i, 0) +1
+    #         if i in hash:
+    #             hash[i] +=1
+    #         else:
+    #             hash[i] =1    
+    #     major = len(nums)//2
+    #     for i, v in hash.items():
+    #         if v>major:
+    #             return i
+        cnt = 0
+        major_element=None
         for i in nums:
-            #hash[i] = hash.get(i, 0) +1
-            if i in hash:
-                hash[i] +=1
+            if cnt == 0:
+                major_element = i
+                cnt+=1
+            elif major_element == i:
+                cnt+=1
             else:
-                hash[i] =1
-
-
-        
-            
-        major = len(nums)//2
-        for i, v in hash.items():
-            if v>major:
-                return i
-        
+                cnt-=1
+        return major_element
