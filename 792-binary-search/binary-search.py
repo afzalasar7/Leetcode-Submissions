0048043eb183770a5374bcbb7943b347139ae2a5
AFZAL ASAR <111232340+afzalasar7@users.noreply.github.com>
2024-01-03 12:43:29 +0530

    Time: 201 ms (87.54%) | Memory: 18.7 MB (16.68%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -7,21 +7,21 @@ class Solution:
         if arr[mid] == target:
             return mid
         elif arr[mid] > target:
-            self.recursive_bin_search(arr, target, mid-1, low)
+            return self.recursive_bin_search(arr, target, mid-1, low)
         else:
-            self.recursive_bin_search(arr, target, high, mid+1)
+            return self.recursive_bin_search(arr, target, high, mid+1)
 
     def search(self, nums: List[int], target: int) -> int:
-        start , end = 0, len(nums)-1
-        while start<=end:
-            mid = (start + end) //2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] > target:
-                end = mid -1
-            elif nums[mid] < target:
+        # start , end = 0, len(nums)-1
+        # while start<=end:
+        #     mid = (start + end) //2
+        #     if nums[mid] == target:
+        #         return mid
+        #     elif nums[mid] > target:
+        #         end = mid -1
+        #     elif nums[mid] < target:
                 
-                start= mid+1
-        return -1
-        #return (self.recursive_bin_search(nums, target, len(nums)-1, 0))
+        #         start= mid+1
+        # return -1
+        return (self.recursive_bin_search(nums, target, len(nums)-1, 0))
         
