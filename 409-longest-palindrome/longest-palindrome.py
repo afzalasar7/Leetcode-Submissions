4081ee24e38df1ca2fc0fd74542fac9003c116e2
AFZAL ASAR <111232340+afzalasar7@users.noreply.github.com>
2024-01-24 10:40:56 +0530

    Time: 25 ms (99.13%) | Memory: 16.6 MB (58.63%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -6,21 +6,18 @@ class Solution:
             counter[i] = counter.get(i,0) +1
         maxi_palindrome=0
 
+        has_single_char = False
+
         for key, value in counter.items():
-            if value%2==0:
+            if value % 2 == 0:
                 maxi_palindrome += value
-                counter[key] = 0
-
             else:
-                if value>2:
-                    maxi_palindrome += value-1
-                    counter[key] = 1
+                maxi_palindrome += value - 1
+                has_single_char = True
 
+        if has_single_char:
+            maxi_palindrome += 1
 
-        for value in counter.values():
-            if value == 1:
-                maxi_palindrome +=1
-                break
         return maxi_palindrome
 
 
