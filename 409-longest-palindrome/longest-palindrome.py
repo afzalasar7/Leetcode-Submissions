81fe13d1f9b564fa329f20875210d824ed4de645
AFZAL ASAR <111232340+afzalasar7@users.noreply.github.com>
2024-02-19 11:35:05 +0530

    Time: 43 ms (24.01%) | Memory: 16.6 MB (68.68%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -1,17 +1,27 @@
+# class Solution:
+#     def longestPalindrome(self, s: str) -> int:
+        # counter = {}
+        # has_odd_char = False
+        # ans = 0
+        # for i in s:
+        #     counter[i] = counter.get(i, 0) +1
+
+        # for v in counter.values():
+        #     if v % 2 == 0:
+        #         ans += v
+        #     else:
+        #         ans += v-1
+        #         has_odd_char = True
+        # return ans+1 if has_odd_char else ans
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        counter = {}
-        has_odd_char = False
-        ans = 0
-        for i in s:
-            counter[i] = counter.get(i, 0) +1
-
-        for v in counter.values():
-            if v % 2 == 0:
-                ans += v
-            else:
-                ans += v-1
-                has_odd_char = True
-        return ans+1 if has_odd_char else ans
-
+        counts = Counter(s)
+        pairs=0
+        extra=0
+        
+        for count in counts.values():
+            pairs += count // 2
+            extra += count % 2
+            
+        return 2 * pairs + (1 if extra else 0)
 
