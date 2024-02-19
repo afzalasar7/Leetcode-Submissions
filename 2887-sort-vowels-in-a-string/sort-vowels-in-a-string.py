95922bde1c647e2e9f3c7556762ddfcb66b789fd
AFZAL ASAR <111232340+afzalasar7@users.noreply.github.com>
2024-02-19 20:24:41 +0530

    Time: 115 ms (84.86%) | Memory: 20.6 MB (37.90%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.py
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.py
@@ -1,15 +1,17 @@
 class Solution:
     def sortVowels(self, s: str) -> str:
-        # Step 1: Collect vowels and sort them in descending order
-        vowels_sorted = sorted([c for c in s if c.lower() in 'aeiou'], reverse=True)
-
-        # Step 2: Construct the answer string by replacing vowels in sorted order
-        result = []
+        ans = []
+        vowel_temp=[]
         for char in s:
-            if char.lower() in 'aeiou':
-                result.append(vowels_sorted.pop())
+            if char in "aeiouAEIOU":
+                vowel_temp.append(char)
+                ans.append("#")
             else:
-                result.append(char)
-
-        # Step 3: Join the characters to form the final string
-        return ''.join(result)              
+                ans.append(char)
+        vowel_temp.sort()
+        ptr=0
+        for i in range(len(ans)):
+            if ans[i] == "#":
+                ans[i] = vowel_temp[ptr]
+                ptr+=1
+        return "".join(ans)
