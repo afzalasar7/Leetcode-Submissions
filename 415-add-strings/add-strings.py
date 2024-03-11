d2c1126d23d0b6f42281ed9511d3e8e9f783669e
AFZAL ASAR <111232340+afzalasar7@users.noreply.github.com>
2024-03-11 11:27:59 +0530

    Time: 53 ms (9.35%) | Memory: 16.8 MB (69.64%) - LeetSync

--- 415-add-strings/add-strings.py
+++ 415-add-strings/add-strings.py
@@ -5,8 +5,10 @@ class Solution:
         i = len(num1)-1
         j = len(num2) -1
         while i >=0 or j >=0 or carry:
-            digit1 = ord(num1[i]) - ord("0") if i >= 0 else 0
-            digit2 = ord(num2[j]) - ord("0") if j >= 0 else 0
+            # digit1 = ord(num1[i]) - ord("0") if i >= 0 else 0
+            # digit2 = ord(num2[j]) - ord("0") if j >= 0 else 0
+            digit1 = int(num1[i]) if i >= 0 else 0
+            digit2 = int(num2[j]) if j >= 0 else 0
             sumOfDigit = digit1 + digit2 + carry
             carry = sumOfDigit // 10
             # result = chr(sumOfDigit % 10 + ord("0")) + result
