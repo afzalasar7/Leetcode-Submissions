775494a4bea3bea77df67e5c5ce9e4dc8141a1ca
AFZAL ASAR <111232340+afzalasar7@users.noreply.github.com>
2024-03-11 11:24:04 +0530

    Time: 40 ms (70.55%) | Memory: 16.6 MB (86.92%) - LeetSync

--- 415-add-strings/add-strings.py
+++ 415-add-strings/add-strings.py
@@ -2,19 +2,14 @@ class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
         result = ""
         carry = 0
-        i = len(num1) - 1
-        j = len(num2) - 1
-
-        # Iterate until either string is exhausted or there's no carry
-        while i >= 0 or j >= 0 or carry:
-            digit1 = ord(num1[i]) - ord('0') if i >= 0 else 0
-            digit2 = ord(num2[j]) - ord('0') if j >= 0 else 0
-            sum_of_digits = digit1 + digit2 + carry
-
-            carry = sum_of_digits // 10
-            result = chr(sum_of_digits % 10 + ord('0')) + result  # Prepend digit
-
-            i -= 1
-            j -= 1
-
+        i = len(num1)-1
+        j = len(num2) -1
+        while i >=0 or j >=0 or carry:
+            digit1 = ord(num1[i]) - ord("0") if i >= 0 else 0
+            digit2 = ord(num2[j]) - ord("0") if j >= 0 else 0
+            sumOfDigit = digit1 + digit2 + carry
+            carry = sumOfDigit // 10
+            result = chr(sumOfDigit % 10 + ord("0")) + result 
+            i -=1
+            j -=1
         return result
