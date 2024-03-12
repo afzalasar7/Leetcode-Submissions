6d1ef7cb440b6dd2491193c8d36befe67fa72274
AFZAL ASAR <111232340+afzalasar7@users.noreply.github.com>
2024-03-12 10:32:57 +0530

    Time: 47 ms (55.08%) | Memory: 16.6 MB (74.40%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,41 +1,13 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        value={"I":1,
-        "V" : 5,
-        "X":10,
-        "L":50,
-        "C" :100,
-        "D":500,
-        "M":1000,
-        "IV":4,
-        "IX":9,
-        "XL":40,
-        "XC":90,
-        "CD":400,
-        "CM":900
-        }
-
-        doubleChar = ["IV",
-        "IX",
-        "XL",
-        "XC",
-        "CD",
-        "CM"]
+        value={"I":1,"V":5,"X":10,"L":50,"C":100, "D":500,"M":1000}
+        
         sum = 0
-        # for item in doubleChar:
-        #     if item in s:
-        #         sum+=value[item]
-        #         s = s.replace(item, "")
-        # for char in s:
-        #     sum+= value[char]
-        i=0
-        l= len(s)-1
-        while i < l:
+        for i in range(len(s)-1):
             if value[s[i]] < value[s[i+1]]:
                 sum-=value[s[i]]
             else:
                 sum+= value[s[i]]
-            i+=1
         sum+=value[s[-1]]
 
         return sum
